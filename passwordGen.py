171ae9284fc02c256325bfa8e8508313a3703d9d
Dorukhan Bekdur <dorukhanbekdur@gmail.com>
2024-06-07 21:10:50 +0300

    Files Uploaded

---
+++ passwordGen.py
@@ -0,0 +1,37 @@
+import string
+import random
+
+length = int(input("Enter password length: "))
+
+print('''Choose character set for password from these :
+      1. Digits
+      2. Letters
+      3. Special characters
+      4. Exit''')
+
+characterList = ""
+
+while True:
+    choice = int(input("Pick a number "))
+    
+    if choice == 1:
+        characterList += string.digits
+    elif choice == 2:
+        characterList += string.ascii_letters
+    elif choice == 3:
+        characterList += string.punctuation
+    elif choice == 4:
+        break
+    else:
+        print("Invalid choice. Please choose a valid option.")
+
+if not characterList:
+    print("No character sets chosen. Exiting...")
+else:
+    password = []
+
+    for i in range(length):
+        randomchar = random.choice(characterList)
+        password.append(randomchar)
+
+    print("The random password is " + "".join(password))
